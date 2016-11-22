a97c281749d1fa20a9767cf40290c656f41fbd9f
Erik Vesteraas <erik@vestera.as>
2016-11-23 00:40:36 +0100

    Add installations to brew.sh

--- brew.sh
+++ brew.sh
@@ -1,15 +1,30 @@
 # First, run whatever http://brew.sh says
 
+brew install the_silver_searcher
+brew install wget
+brew install rlwrap
+brew install python
+brew install python3
+
 brew tap caskroom/cask
 brew cask install atom
 brew cask install iterm2
 brew cask install google-chrome
 brew cask install spotify
+brew cask install nvalt
+brew cask install slack
+# brew cask install haskell-platform
 
-curl https://sh.rustup.rs -sSf | sh
+# curl https://sh.rustup.rs -sSf | sh
 
-chsh -s /bin/zsh
+# chsh -s /bin/zsh
 
 apm install file-icons
 apm install editorconfig
 apm install expand-region
+apm install less-than-slash
+apm install set-syntax
+
+apm install language-haskell
+apm install language-rust
+apm install language-scala
