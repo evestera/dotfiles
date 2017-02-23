9f5af0641682338671a3ef52b827ab54f0365440
Erik Vesteraas <erik@vestera.as>
2017-02-23 01:06:54 +0100

    Add file for zsh functions (++)

---
+++ zfunctions.zsh
@@ -0,0 +1,44 @@
+function mkcd() {
+  mkdir -p "$@" && cd ${@: -1};
+}
+
+function wtf_header() {
+  echo "\x1b[34m${1}:\x1b[0m"
+}
+
+function wtf_echo() {
+  echo $1 |& sed 's/^/  /'
+}
+
+function wtf() {
+  wtf_header "type";
+  wtf_echo "$(whence -vas $1)";
+  local FULLPATH=$(which $1);
+  if [[ $FULLPATH =~ ^/ ]]; then
+    wtf_header "ls";
+    wtf_echo "$(CLICOLOR_FORCE=1 ls -alhG $FULLPATH)";
+    wtf_header "file";
+    wtf_echo "$(file -b $FULLPATH)";
+  fi
+  wtf_header "whatis";
+  wtf_echo "$(whatis $1 | grep "^$1" | head -3)";
+  if (( $+commands[brew] )); then
+    wtf_header "brew";
+    if [[ $FULLPATH =~ ^/ ]]; then
+      local FORMULA=${${${$(realpath $FULLPATH)##*/Cellar/}%%/*}:-$1}
+    else
+      local FORMULA=$1
+    fi
+    if (( $+commands[jq] )); then
+      local JQ='
+        "Formula: \(.[0].name)",
+        .[0].desc,
+        "Stable: \(.[0].versions.stable) Installed: \(.[0].installed[-1].version)",
+        "Homepage: \(.[0].homepage)"
+      ';
+      wtf_echo "$(brew info --json=v1 $FORMULA | jq -r $JQ)";
+    else
+      wtf_echo "$(brew info $FORMULA | head -4)";
+    fi
+  fi
+}
--- zshrc
+++ zshrc
@@ -3,7 +3,8 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
-unsetopt correct
+unsetopt correct # don't try to autocorrect in shell
+setopt append_create # if I try >>foo.txt and foo.txt does not exist, create it
 
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/dotfiles/bin:$PATH"
@@ -12,11 +13,14 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export VISUAL=vim
 export EDITOR=vim
 
+source "$HOME/dotfiles/zfunctions.zsh"
+
 alias vor='ssh erikadr@vor.ifi.uio.no'
 alias yse='ssh 95.215.45.137'
 
 alias sml='PARSER_PRIMARY_PROMPT="$ " PARSER_SECONDARY_PROMPT=">   " rlwrap -p red sml'
 alias dotf='atom ~/dotfiles'
+alias cdotf='cd ~/dotfiles'
 
 alias java8='export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
 alias java9='export JAVA_HOME=$(/usr/libexec/java_home -v 9)'
