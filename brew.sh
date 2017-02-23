96f54dfa943e4257327524cbb2c15b2cceecacf2
Erik Vesteraas <erik@vestera.as>
2017-02-23 22:35:34 +0100

    Some additions to brew.sh

--- brew.sh
+++ brew.sh
@@ -6,6 +6,10 @@ fi
 brew install the_silver_searcher
 brew install wget
 brew install rlwrap
+brew install tree
+brew install atool
+brew install jq
+brew install watchexec
 brew install python
 brew install python3
 
@@ -16,11 +20,12 @@ brew cask install google-chrome
 brew cask install spotify
 brew cask install nvalt
 brew cask install slack
-# brew cask install haskell-platform
+brew cask install spectacle
 
-# curl https://sh.rustup.rs -sSf | sh
-
-# chsh -s /bin/zsh
+# Quick Look plugins
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
 
 apm install file-icons
 apm install editorconfig
@@ -31,3 +36,4 @@ apm install set-syntax
 apm install language-haskell
 apm install language-rust
 apm install language-scala
+apm install language-latex
