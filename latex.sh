041938a405a221239852f67ef85cce79c857b149
Erik Vesteraas <erik@vestera.as>
2017-02-23 01:01:23 +0100

    Save my LaTeX setup

---
+++ latex.sh
@@ -0,0 +1,19 @@
+brew cask install basictex
+
+sudo tlmgr update --self
+sudo tlmgr install graphics-def
+sudo tlmgr install minted
+sudo tlmgr install fvextra
+sudo tlmgr install ifplatform
+sudo tlmgr install xstring
+sudo tlmgr install framed
+sudo tlmgr install dirtytalk
+sudo tlmgr install luatex85
+sudo tlmgr install biblatex
+sudo tlmgr install biber
+sudo tlmgr install logreq
+
+# PDF viewer that auto-refreshes
+brew cask install skim
+# For jumping between Atom and Skim
+apm install git@github.com:Corwynt/skim-displayline.git
